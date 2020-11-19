fc32b42e580961000a99ce1ee7147f38902421f0
Milton Boos Junior <junior_boos@live.com>
2020-11-19 22:56:15 +0000

    activates animation when button click

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="up">
-        <div className="triangle"></div>
-        <div className="block"></div>
+   const [open, setOpen] = useState(false);
+
+   return (
+      <div className="App">
+         <div className={open ? "up activate-open-up" : "up"}>
+            <div className="triangle"></div>
+            <div className="block"></div>
+         </div>
+         <div className={open ? "down activate-open-down" : "down"}>
+            <div className="block"></div>
+            <div className="triangle"></div>
+         </div>
+         <button className="button" onClick={() => setOpen(!open)}>
+            Activate
+         </button>
       </div>
-      <div className="down">
-        <div className="block"></div>
-        <div className="triangle"></div>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default App;
