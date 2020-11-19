85606d14bd98bf059a52a49cf5f84dbcc21cb129
Milton Boos Junior <junior_boos@live.com>
2020-11-19 23:37:37 +0000

    added close box animation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import React, { useState } from "react";
 
 function App() {
    const [open, setOpen] = useState(false);
+   const [first, setFirst] = useState(true);
+
+   const switchPosition = () => {
+      setFirst(false);
+      setOpen(!open);
+      console.log(!open);
+   };
 
    return (
       <div className="App">
-         <div className={open ? "up activate-open-up" : "up"}>
+         <div
+            className={
+               !first
+                  ? open
+                     ? "up activate-open-up"
+                     : "up activate-close-up"
+                  : "up"
+            }
+         >
             <div className="triangle"></div>
             <div className="block"></div>
          </div>
@@ -14,7 +29,7 @@ function App() {
             <div className="block"></div>
             <div className="triangle"></div>
          </div>
-         <button className="button" onClick={() => setOpen(!open)}>
+         <button className="button" onClick={switchPosition}>
             Activate
          </button>
       </div>
